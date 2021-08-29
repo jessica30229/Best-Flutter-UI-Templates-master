9d2996bbfd61d70c985f64b57d95fc878e325252
helenchiu1221 <56269497+helenchiu1221@users.noreply.github.com>
2021-08-29 23:08:34 +0800

    Initial commit

---
+++ best_flutter_ui_templates/lib/introduction_animation/components/splash_view.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class SplashView extends StatefulWidget {
+  final AnimationController animationController;
+
+  const SplashView({Key? key, required this.animationController})
+      : super(key: key);
+
+  @override
+  _SplashViewState createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  Widget build(BuildContext context) {
+    final _introductionanimation =
+        Tween<Offset>(begin: Offset(0, 0), end: Offset(0.0, -1.0))
+            .animate(CurvedAnimation(
+      parent: widget.animationController,
+      curve: Interval(
+        0.0,
+        0.2,
+        curve: Curves.fastOutSlowIn,
+      ),
+    ));
+    return SlideTransition(
+      position: _introductionanimation,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Image.asset(
+                'assets/introduction_animation/introduction_image.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 8.0, bottom: 8.0),
+              child: Text(
+                "ünidust",
+                style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 64, right: 64),
+              child: Text(
+                "以數據事實與遊戲化app改善家務性別分⼯不均之問題",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16.0,),
+              ),
+            ),
+            SizedBox(
+              height: 48,
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  bottom: MediaQuery.of(context).padding.bottom + 16),
+              child: InkWell(
+                onTap: () {
+                  widget.animationController.animateTo(0.2);
+                },
+                child: Container(
+                  height: 58,
+                  padding: EdgeInsets.only(
+                    left: 56.0,
+                    right: 56.0,
+                    top: 16,
+                    bottom: 16,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(38.0),
+                    color: Color(0xFFFFD84C),
+                  ),
+                  child: Text(
+                    "開始體驗吧",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
